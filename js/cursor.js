a9ec53ca72da2426e28bc33181018c787aea1558
Leonor Tejo <72199967+LeonorTejo@users.noreply.github.com>
2022-01-02 18:15:40 +0000

    final

--- js/cursor.js
+++ js/cursor.js
@@ -1,41 +1,34 @@
-const bigBall = document.querySelector('.cursorBig');
-const smallBall = document.querySelector('.cursorSmall');
-const hoverables = document.querySelectorAll('.hoverable');
+const cursor = document.querySelector('.cursor');
+const $hoverables = document.querySelectorAll('.hoverable');
+
+//const hoverables = document.querySelectorAll('.hoverable');
 
 // Listeners
 document.body.addEventListener('mousemove', onMouseMove);
 
-for (let i = 0; i < hoverables.length; i++) {
-  hoverables[i].addEventListener('mouseenter', onMouseHover);
-  hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+
+for (let i = 0; i < $hoverables.length; i++) {
+  $hoverables[i].addEventListener('mouseenter', onMouseHover);
+  $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
 }
 
 // Move the cursor
 function onMouseMove(e) {
-  TweenMax.to(bigBall, .4, {
-    x: e.clientX ,
-    y: e.clientY
-  })
-  TweenMax.to(smallBall, .1, {
+  TweenMax.to(cursor, .1, {
     x: e.clientX - 5,
-    y: e.clientY - 5
+    y: e.clientY - 7
   })
 }
 
+
 // Hover an element
 function onMouseHover() {
-  TweenMax.to(smallBall, .3, {
-    scale: 6
-  })
-  TweenMax.to(bigBall, .3, {
-    opacity: 0
+  TweenMax.to(cursor, .3, {
+    scale: 4
   })
 }
 function onMouseHoverOut() {
-  TweenMax.to(smallBall, .3, {
+  TweenMax.to(cursor, .3, {
     scale: 1
-  })
-  TweenMax.to(bigBall, .3, {
-    opacity: 1
   })
 }
--- js/main.js
+++ js/main.js
@@ -1,12 +1,155 @@
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollToPlugin);
 
-gsap.to("progress", {
-  value: 100,
-  ease: "none",
+let container = document.querySelector("main");
+
+
+//---------------elementos iniciais---------------
+let tl1 = gsap.timeline({ delay: 0.5 });
+
+tl1
+  .from(
+    ".first .div1",
+    {
+      y: 50,
+      opacity: 0,
+      ease: "power4",
+      duration: 3,
+    },
+    0.7
+  )
+  .from(
+    ".first .div2",
+    {
+      y: 50,
+      opacity: 0,
+      ease: "power4",
+      duration: 3,
+    },
+    0.7
+  )
+  .from(
+    ".first .div3",
+    {
+      y: 25,
+      opacity: 0,
+      ease: "power2",
+      duration: 5,
+    },
+    0.7
+  )
+  .from(
+    ".first .div4",
+    {
+      y: 25,
+      opacity: 0,
+      ease: "power2",
+      duration: 5,
+    },
+    0.7
+  );
+
+//---------------SEGUNDO---------------
+let tl2 = gsap.timeline({
   scrollTrigger: {
-    trigger: "main",
-    scrub: 0.3,
+    toggleActions: "restart pause resume pause",
+    trigger: ".second",
+    start: "top bottom",
   },
 });
 
-//TEXTO THIRD
+tl2
+  .from(
+    ".second .div1",
+    {
+      y: 40,
+      opacity: 0,
+      ease: "power4",
+      duration: 3,
+    },
+    0.7
+  )
+  .from(
+    ".second .div2",
+    {
+      y: 40,
+      opacity: 0,
+      ease: "power4",
+      duration: 3,
+    },
+    0.7
+  )
+  .from(
+    ".second .div3",
+    {
+      y: 40,
+      opacity: 0,
+      ease: "power4",
+      duration: 3,
+    },
+    0.7
+  )
+  .from(
+    ".second .div5",
+    {
+      y: 40,
+      opacity: 0,
+      ease: "power4",
+      duration: 3,
+    },
+    0.7
+  );
+
+var el = document.querySelectorAll(".el");
+TweenMax.set(el, { autoAlpha: 0, right: "0%", xPercent: "-50%" });
+var tl2_ = new TimelineMax({ repeat: -1 });
+
+for (var i = 0; i < el.length; i++) {
+  E = el[i];
+  tl2_
+    .to(E, { autoAlpha: 0, right: "5%", ease: Power1.easeIn })
+    .to(E, 1, { autoAlpha: 1, right: "15%", ease: Power0.easeNone })
+    .to(E, 2, { right: "30%", autoAlpha: 0, ease: Power1.easeOut });
+}
+
+//---------------TERCEIRO---------------
+var texto3 = document.getElementsByClassName("textP");
+for (var i = 0; i < texto3.length; i++) {
+  texto3[i].style.opacity = 0;
+}
+
+let tl3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".second",
+    scrub: 1,
+    start: "bottom center",
+  },
+});
+
+tl3.to(".third .textP", {
+  y: 50,
+  opacity: 1,
+  ease: "power4",
+  duration: 3,
+});
+
+//---------------FINAL---------------
+
+let tl4 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".third",
+    start: "bottom 70%",
+    scrub:1,
+  },
+});
+
+tl4.from(
+  ".last",
+  {
+    y: 40,
+    opacity: 0,
+    ease: "power4",
+    duration: 3,
+  },
+  0.7
+);
